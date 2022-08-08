50bdbc6bdc143d2e45d6e4e00ec181381faa4e68
LizzyMcKay <user@server.fake>
2022-08-08 18:18:40 +0200

    updated annotation of script

--- quicksort.sh
+++ quicksort.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # a script to move graphs of experimental sessions to corresponding folders
 # !!! put in dir with graph images !!!
+# !!! ALWAYS check for ERRORS, especially with A_CONTROL sessions
 
 list=("A1" "A2" "Acontr" "A" "B1" "B2" "B")
 
